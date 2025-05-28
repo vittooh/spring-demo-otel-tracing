4f98ed9328e3bcd0205d7ff8e5f5fa67743b82a3
Vitor <vitorhugosilva2009@hotmail.com>
2025-05-28 18:35:19 -0300

    files

---
+++ src/test/kotlin/com/dbl/spring_demo_otel_tracing/BasicTest4.kt
@@ -0,0 +1,13 @@
+package com.dbl.spring_demo_otel_tracing
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import kotlin.test.fail
+
+class BasicTest4 {
+
+    @Test
+    fun potato(){
+        Assertions.assertTrue(false)
+    }
+}
